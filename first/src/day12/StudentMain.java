45390cfc9f4c79100866073ecdb93da62b4ff091
user <user@DESKTOP-F4T27T3>
2021-04-14 13:04:51 +0900

    Student클래스의 메소드 예제 추가활용

--- first/src/day12/StudentMain.java
+++ first/src/day12/StudentMain.java
@@ -18,8 +18,41 @@ public class StudentMain {
 		System.out.println(stu2.stuNum);
 		System.out.println(stu2); //주소가 나옴 결과 : day12.Student@7637f22 가 나옴
 		
+		System.out.println("메소드 호출 전");
 		
-
+		//fieldPrint메소드 호출
+		stu1.fieldPrint();
+		//형식 : 객체이름.메소드이름();
+		//stu1의 변수의 값이 출력되고 이용이 된다.
+		
+		stu2.fieldPrint();
+		
+		System.out.println("리턴이 있는 메소드 호출");
+		//리턴이 있는 메소드 호출
+		//호출만 하고 끝이 아니라 리턴값을 활용하는 내용이 필요하다.
+		int aa = 0;
+		System.out.println(aa);
+		aa = stu1.numberMethod();
+		System.out.println(aa);
+		//aa.numberMethod(); -> aa라는 변수는 Student클래스로 선언한 변수가 아니기 때문에 사용이 되지 않는다.
+		
+		System.out.println(stu1.numberMethod()); //numberMethod메소드의 리턴값인 num변수가 있기때문에 10이 출력이 된다.
+		
+		System.out.println("String메소드 호츨");
+		System.out.println(stu2.stringMethod());
+		
+		String bb = stu2.stringMethod();
+		System.out.println(bb);
+		
+		//매개변수 있는 메소드 호출
+		Student stu4 = new Student();
+		int cc = stu4.numberMethod2(30);
+		System.out.println(cc);
+		
+		int number1 = 50;
+		cc = stu4.numberMethod2(number1);
+		//매개변수 안에 변수명을 넣을 수 있다.
+		System.out.println(cc);
 	}
 
 }
