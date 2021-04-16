eb64d0946cffb8ec84d568d564fd46da6e67124b
user <user@DESKTOP-F4T27T3>
2021-04-16 14:44:14 +0900

    줄 정리

--- first/src/day14/People.java
+++ first/src/day14/People.java
@@ -1,25 +1,25 @@
 package day14;
 
 public class People {
-	//private로 필드 선언
+	// private로 필드 선언
 	private String name, rNumber, address;
 	private int age;
-	
-	//기본 생성자 선언
-	People(){
-		
+
+	// 기본 생성자 선언
+	People() {
+
 	}
-	
-	//마우스 오른쪽버튼 -> 소스 -> 제너럴컨스트럭톨 유징 필드 => 생성자 선언해줌
-	//모든 필드를 매개변수로 이용하는 생성자 선언
-	People(String name, String rNumber, String address, int age){
+
+	// 마우스 오른쪽버튼 -> 소스 -> 제너럴컨스트럭톨 유징 필드 => 생성자 선언해줌
+	// 모든 필드를 매개변수로 이용하는 생성자 선언
+	People(String name, String rNumber, String address, int age) {
 		this.name = name;
 		this.rNumber = rNumber;
 		this.address = address;
 		this.age = age;
 	}
 
-	//Getter/Setter 선언
+	// Getter/Setter 선언
 	public String getName() {
 		return name;
 	}
@@ -52,12 +52,10 @@ public class People {
 		this.age = age;
 	}
 
-	//toString메소드 : 객체의 필드값을 정돈해줌, 객체만 선언해도 해당 객체의 각 필드값을 정돈해준다.
+	// toString메소드 : 객체의 필드값을 정돈해줌, 객체만 선언해도 해당 객체의 각 필드값을 정돈해준다.
 	@Override
 	public String toString() {
 		return "People [name=" + name + ", rNumber=" + rNumber + ", address=" + address + ", age=" + age + "]";
 	}
-	
-	
-		
-	}
+
+}
--- first/src/day14/PeopleMain.java
+++ first/src/day14/PeopleMain.java
@@ -5,47 +5,45 @@ import java.util.*;
 public class PeopleMain {
 
 	public static void main(String[] args) {
-		//People클래스를 사용하는 main클래스
-		
+		// People클래스를 사용하는 main클래스
+
 		People peo1 = new People();
-		People peo2 = new People("이름2", "111111-1111111", "주소2",20);
+		People peo2 = new People("이름2", "111111-1111111", "주소2", 20);
 		People peo3 = new People();
-		
+
 		peo1.setName("이름1");
 		peo1.setrNumber("000000-0000000");
 		peo1.setAddress("주소1");
 		peo1.setAge(10);
-		
+
 		peo3.setName("이름3");
 		peo3.setrNumber("222222-2222222");
 		peo3.setAddress("주소3");
 		peo3.setAge(30);
-		
+
 		List<People> peopeleList = new ArrayList<People>();
-		
+
 		peopeleList.add(peo1);
 		peopeleList.add(peo2);
 		peopeleList.add(peo3);
-		
-		for(int i=0; i<peopeleList.size(); i++) {
+
+		for (int i = 0; i < peopeleList.size(); i++) {
 			System.out.println("이름 : " + peopeleList.get(i).getName());
-			System.out.println("주민번호 : " +peopeleList.get(i).getrNumber());
-			System.out.println("주소 : " +peopeleList.get(i).getAddress());
-			System.out.println("나이 : " +peopeleList.get(i).getAge());
+			System.out.println("주민번호 : " + peopeleList.get(i).getrNumber());
+			System.out.println("주소 : " + peopeleList.get(i).getAddress());
+			System.out.println("나이 : " + peopeleList.get(i).getAge());
 			System.out.println();
 		}
 		System.out.println();
-		
-		//toString메소드를 선언하여서 객체의 각 필드값의 값이 출력이 된다.
+
+		// toString메소드를 선언하여서 객체의 각 필드값의 값이 출력이 된다.
 		System.out.println(peopeleList.get(0));
 		System.out.println(peo1);
 		System.out.println();
-		
-		for(int i=0; i<peopeleList.size(); i++) {
+
+		for (int i = 0; i < peopeleList.size(); i++) {
 			System.out.println(peopeleList.get(i));
 		}
-		
-		
 
 	}
 
